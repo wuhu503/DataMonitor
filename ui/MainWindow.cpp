8ac2a2a1c151e22f1a9368fd546d7468bf2964af
wuhu503 <2370498393@qq.com>
2026-07-09 11:18:14 +0800

    v3: 实时数据显示 — 曲线图表 + 数据表格
    - 添加 Qt Charts 实时曲线 (QLineSeries + QValueAxis)
    - 实现数据点自动滚动显示 (窗口 200 点)
    - 添加 QTableView 数据表格 (序号/地址/数值/时间)
    - 重构 MainWindow 初始化 (initMenuBar / initWorker / initChart / initTable)
    - 状态栏显示串口错误信息

--- ui/MainWindow.cpp
+++ ui/MainWindow.cpp
@@ -1,6 +1,9 @@
-#include "MainWindow.h"
+﻿#include "MainWindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QVBoxLayout>
+#include <QtCharts>
+#include <QDateTime>
 #include "ui/SettingsDialog.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -9,12 +12,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     initMenuBar();
+    initWorker();
+    initChart();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    if (m_workerThread->isRunning()) {
+    if (m_workerThread && m_workerThread->isRunning()) {
         m_worker->stop();
         m_workerThread->wait(3000);
     }
@@ -22,11 +27,6 @@ MainWindow::~MainWindow()
 
 void MainWindow::initMenuBar()
 {
-    // 创建通信线程
-    m_workerThread = new QThread(this);
-    m_worker = new CommWorker();
-    m_worker->moveToThread(m_workerThread);
-
     connect(ui->actionExit,&QAction::triggered,qApp, &QApplication::quit);
     connect(ui->actionAbout,&QAction::triggered,this,[this](){
         QMessageBox::about(this,"关于 DataMonitor","工业数据采集与监控系统 v1.0");
@@ -56,20 +56,87 @@ void MainWindow::initMenuBar()
         dialog->show();
     });
     connect(ui->actionExportData,&QAction::triggered,this,[this](){
-        QMessageBox::information(this,"提示","导出功能将在v4版本中实现!");
+        QMessageBox::information(this,"提示","导出功能将在v4版本中实现");
     });
+}
 
-    // 接收跨线程信号
+void MainWindow::initWorker()
+{
+    m_workerThread = new QThread(this);
+    m_worker = new CommWorker();
+    m_worker->moveToThread(m_workerThread);
+
     connect(m_worker, &CommWorker::frameReceived, this, [this](const ParsedFrame &frame) {
-        // 收到一帧数据，后续这里更新 UI
+        if (!m_series) return;
+        m_series->append(++m_pointCount, frame.value);
+
+        // 插入表格行
+        QList<QStandardItem *> row;
+        row << new QStandardItem(QString::number(m_pointCount))
+            << new QStandardItem("0x" + QString::number(frame.address, 16))
+            << new QStandardItem(QString::number(frame.value))
+            << new QStandardItem(QDateTime::currentDateTime().toString("HH:mm:ss.zzz"));
+        m_tableModel->insertRow(0, row);
+
+        // 最多保留100行
+        while (m_tableModel->rowCount() > 100) {
+            m_tableModel->removeRow(m_tableModel->rowCount() - 1);
+        }
+
+        if (m_pointCount > 200) {
+            auto xAxes = m_chart->axes(Qt::Horizontal);
+            if (!xAxes.isEmpty()) {
+                auto *xAxis = qobject_cast<QValueAxis *>(xAxes.first());
+                if (xAxis) xAxis->setRange(m_pointCount - 200, m_pointCount);
+            }
+        }
     });
 
     connect(m_worker, &CommWorker::errorOccurred, this, [this](const QString &msg) {
-        // 串口错误时显示到状态栏
+        ui->statusbar->showMessage(msg, 5000);
     });
 
     connect(m_workerThread, &QThread::started, m_worker, &CommWorker::start);
     connect(m_worker, &CommWorker::finished, m_workerThread, &QThread::quit);
 
     m_workerThread->start();
+}
+
+void MainWindow::initChart()
+{
+    m_series = new QLineSeries();
+    m_series->setName("通道1");
+
+    m_chart = new QChart();
+    m_chart->addSeries(m_series);
+    m_chart->setTitle("实时数据曲线");
+    m_chart->setAnimationOptions(QChart::SeriesAnimations);
+
+    auto *axisX = new QValueAxis();
+    axisX->setTitleText("采样点");
+    axisX->setRange(0, 100);
+    m_chart->addAxis(axisX, Qt::AlignBottom);
+    m_series->attachAxis(axisX);
+
+    auto *axisY = new QValueAxis();
+    axisY->setTitleText("数值");
+    axisY->setRange(0, 1000);
+    m_chart->addAxis(axisY, Qt::AlignLeft);
+    m_series->attachAxis(axisY);
+
+    auto *chartView = new QChartView(m_chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    auto *tab1 = ui->tabWidget->widget(0);
+    auto *layout = new QVBoxLayout(tab1);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->addWidget(chartView);
+}
+
+void MainWindow::initTable()
+{
+    m_tableModel = new QStandardItemModel(0, 4, this);
+    m_tableModel->setHorizontalHeaderLabels({"序号", "地址", "数值", "时间"});
+    ui->tableView->setModel(m_tableModel);
+    ui->tableView->horizontalHeader()->setStretchLastSection(true);
 }
--- ui/MainWindow.h
+++ ui/MainWindow.h
@@ -1,8 +1,15 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
 #include <QThread>
+
+#include <QChart>
+#include <QChartView>
+#include <QLineSeries>
+#include <QValueAxis>
+#include <QStandardItemModel>
+
 #include "thread/CommWorker.h"
 
 QT_BEGIN_NAMESPACE
@@ -21,7 +28,16 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
     void initMenuBar();
+    void initWorker();
+    void initChart();
+    void initTable();
+
+    QLineSeries *m_series = nullptr;
+    QChart *m_chart = nullptr;
+    int m_pointCount = 0;
+    QStandardItemModel *m_tableModel = nullptr;
 
     QThread *m_workerThread = nullptr;
     CommWorker *m_worker = nullptr;
